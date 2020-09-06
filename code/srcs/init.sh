f80ef72ffecba1981a46a26d98bfa9dbe9a59626
paikwiki <paikwiki@gmail.com>
2020-09-06 21:46:30 +0900

    Create a database for wordpress with Mariadb

--- code/srcs/init.sh
+++ code/srcs/init.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+service mysql start
+
+echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user='root';" \
+		| mysql -u root --skip-password
+echo "CREATE DATABASE wordpress;" \
+		| mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* to 'root'@'localhost';" \
+		| mysql -u root --skip-password
+echo "FLUSH PRIVILEGES" \
+		| mysql -u root --skip-password
+
+nginx -g 'daemon off;'
