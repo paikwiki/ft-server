8c31ed4882f0df6813715f35cfb5cca44c255d7a
paikwiki <paikwiki@gmail.com>
2020-09-09 12:55:30 +0900

    Fix error: wrong initializing for ROOT_DIR

--- code/srcs/init.sh
+++ code/srcs/init.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-ROOT_DIR = "/var/www/wordpress"
+ROOT_DIR="/var/www/wordpress"
 
 service mysql start
 
 chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
 
-# mkdir ${ROOT_DIR}
-
-echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user='root';" \
-		| mysql -u root --skip-password
+echo "UPDATE mysql.user SET plugin = 'mysql_native_password'
+		WHERE user='root';" | mysql -u root --skip-password
 echo "CREATE DATABASE wordpress;" \
 		| mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* to 'root'@'localhost';" \
@@ -23,4 +21,4 @@ mv -f phpMyAdmin-4.9.5-english ${ROOT_DIR}/phpmyadmin
 mv /var/config.inc.php ${ROOT_DIR}/phpmyadmin/
 
 service php7.3-fpm start
-nginx -g 'daemon off;'
+nginx -g "daemon off;"
